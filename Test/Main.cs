d8d2ce5c9188f5ca5038cceb2fc093742fd27084
xiaodouxd <lianghengming@live.cn>
2023-05-13 19:01:27 +0800

    把 test 加回来

---
+++ Test/Main.cs
@@ -0,0 +1,17 @@
+﻿using IdGen;
+
+try
+{
+    var hashset = new HashSet<long>();
+    for (var i = 0; i < 50000; i++)
+    {
+        var id = IdGen<Snowflake, long>.Gen();
+        if (hashset.Contains(id))
+            throw new Exception("xxx");
+        hashset.Add(id);
+    }
+}
+catch (Exception e) 
+{
+    Console.WriteLine(e.ToString());
+}
